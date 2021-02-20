9c454a1889e3a15f6b157eb638b1200d6a71c5e5
Karamijit <karamjitverma9@gmail.com>
2021-02-21 02:11:42 +0530

    services changes

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -30,8 +30,29 @@ export class TodoService {
       }
     ];
   }
-
+//we need to get the above constructor or precisely the value inside the constructor as a ovservable to that component
+//and that is the presise sreason we are doing this of 
   getTodos(){
-    
+    //now just by osing "OF" operator we have made this array todo as a simple observable
+    return of(this.todos); //"OF" operator make things observable
+  }
+
+  addTodo(todo:Todo){
+    this.todos.push(todo);
+  }
+
+  changeStatus(todo:Todo){
+    this.todos.map(singleTodo => {//this is how you fire up a callback
+      //for every single toDo i wanto check for a condition 
+      if(singleTodo.id==todo.id){
+        todo.isCompleted=!todo.isCompleted; 
+      }
+    });
+  }
+  deleteTodo(todo:Todo){
+    const indexOfTodo=this.todos.findIndex(//this indexoftodo holds the id from the array which matches the id of the todo which you have provided to me
+      (currentObj) =>currentObj.id === todo.id
+    );
+    this.todos.splice(indexOfTodo,1);//delete
   }
 }
