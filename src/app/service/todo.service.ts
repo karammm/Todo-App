1630918921b3d01bdb0c20e817c07f3b0c2e54e3
Karamijit <karamjitverma9@gmail.com>
2021-02-20 15:09:04 +0530

    Services work

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Todo } from './../model/Todo';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todos: Todo[];//todos of type array of type Todo bcz thats why our model was created
+  constructor() { 
+    this.todos=[
+      {
+        id:'111',
+        title: "Learn C++",
+        isCompleted: true,
+        date: new Date()
+      },
+      {
+        id:'112',
+        title: "Learn Java",
+        isCompleted: true,
+        date: new Date()
+      },
+      {
+        id:'113',
+        title: "Learn Angular",
+        isCompleted: false,
+        date: new Date()
+      }
+    ];
+  }
 
-  constructor() { }
+  getTodos(){
+    
+  }
 }
